0808abcfb3aef7d67a994328efae51503b2c6484
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-05-12 12:56:52 +0300

    Genre table create

--- app/src/main/java/com/bsrakdg/moviecentral/models/Genre.java
+++ app/src/main/java/com/bsrakdg/moviecentral/models/Genre.java
@@ -1,17 +1,64 @@
 package com.bsrakdg.moviecentral.models;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Genre {
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-    @SerializedName("id")
-    @Expose
+@Entity(tableName = "genres")
+public class Genre implements Parcelable {
+
+    public static final Creator<Genre> CREATOR = new Creator<Genre>() {
+        @Override
+        public Genre createFromParcel(Parcel in) {
+            return new Genre(in);
+        }
+
+        @Override
+        public Genre[] newArray(int size) {
+            return new Genre[size];
+        }
+    };
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
     private int id;
 
-    @SerializedName("name")
-    @Expose
+    @ColumnInfo(name = "name")
     private String name;
+
+    @ColumnInfo(name = "date")
+    private String date;
+
+    private Genre(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        date = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeInt(id);
+        parcel.writeString(name);
+        parcel.writeString(date);
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 
     public int getId() {
         return id;
@@ -19,13 +66,5 @@ public class Genre {
 
     public String getName() {
         return name;
-    }
-
-    @Override
-    public String toString() {
-        return "Genre{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
     }
 }
